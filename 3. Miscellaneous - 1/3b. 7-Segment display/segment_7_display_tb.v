225c2dd15427a7181f0cb6ab5474ee6cc9bd9849
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-08-17 13:30:14 +0530

    Add files via upload

---
+++ 3. Miscellaneous - 1/3b. 7-Segment display/segment_7_display_tb.v
@@ -0,0 +1,20 @@
+module segment_7_display_tb;
+  reg [3:0] i;
+  wire a, b, c, d, e, f, g;
+  
+  segment_7_display uut (.i(i),.a(a),.b(b),.c(c),.d(d),.e(e),.f(f),.g(g));
+
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    $display("Time | i | a b c d e f g");
+    $display("------------------------");
+
+    for (i = 0; i < 10; i = i + 1) begin
+      #10; 
+      $display("%4dns | %d | %b %b %b %b %b %b %b", $time, i, a, b, c, d, e,f, g);
+    end
+
+    $finish;
+  end
+
+endmodule
