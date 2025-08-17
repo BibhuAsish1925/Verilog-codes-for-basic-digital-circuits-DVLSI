d5d2c335917b961ad018efe200348af6606cab59
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-08-17 09:55:23 +0530

    Add files via upload

---
+++ 2. Adders/2a. Half Adder/Half_adder_design.v
@@ -0,0 +1,47 @@
+// whichever model you want just remove /* */ and change b/w output and output reg
+
+module half_adder(
+  input a,b,
+  output sum, carry, 
+  output reg sum, carry //behvioural only
+  );
+  
+  
+  //dataflow
+  /*assign sum=a^b;
+  assign carry= a&b;*/
+  
+  
+  //behavioural
+  /*always @(a,b)
+    begin 
+      if(a==1'b0 && b==1'b0)
+        begin 
+          sum<=1'b0;
+          carry<=1'b0;
+        end
+      else if(a==1'b0 && b==1'b1)
+        begin 
+          sum<=1'b1;
+          carry<=1'b0;
+        end
+      else if(a==1'b1 && b==1'b0)
+        begin 
+          sum<=1'b1;
+          carry<=1'b0;
+        end
+      else if(a==1'b1 && b==1'b1)
+        begin 
+          sum<=1'b0;
+          carry<=1'b1;
+        end
+    end*/
+  
+  
+  //gatelevel
+  xor s(sum,a,b);
+  and co(carry,a,b);
+             
+endmodule
+
+
