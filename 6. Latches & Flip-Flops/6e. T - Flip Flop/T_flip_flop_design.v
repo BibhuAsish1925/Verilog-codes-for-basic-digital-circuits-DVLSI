f6d4d3c35f0f949fdd8f781c62a1305b401005bf
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-08-19 17:33:27 +0530

    Add files via upload

---
+++ 6. Latches & Flip-Flops/6e. T - Flip Flop/T_flip_flop_design.v
@@ -0,0 +1,23 @@
+module T_flip_flop(
+  input t,
+  input clk,
+  output reg q,q_bar  //for behavioural model
+);
+  
+  initial begin
+    q = 0;
+    q_bar = 1;
+  end
+
+
+  //behavioural model
+  always @(posedge clk)  // specifying positive edge clock pulse
+    
+    begin
+      case({t})
+        1'b0: begin q<=q;  q_bar<=q_bar; end
+        1'b1: begin q<=~q;  q_bar<=~q_bar; end  // very very important and not  q<=q_bar;  q_bar<=q; which is wrong
+      endcase
+    end
+ 
+endmodule
