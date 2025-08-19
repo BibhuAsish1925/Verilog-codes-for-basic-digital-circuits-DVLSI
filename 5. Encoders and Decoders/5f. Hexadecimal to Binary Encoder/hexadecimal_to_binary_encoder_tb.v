0a5b664657c57ed0d9ce56948ed97edc6ada008f
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-08-19 22:30:08 +0530

    Add files via upload

---
+++ 5. Encoders and Decoders/5f. Hexadecimal to Binary Encoder/hexadecimal_to_binary_encoder_tb.v
@@ -0,0 +1,20 @@
+module tb;
+
+  reg  [15:0] in;
+  wire [3:0]  out;
+
+  hexadecimal_binary uut (.in(in),.out(out));
+
+  initial begin
+    $dumpfile("hex_encoder.vcd");
+    $dumpvars(0, tb11);
+
+    in = 16'h0000; #10;
+    in = 16'h8000; #10;
+    in = 16'h0400; #10;
+    in = 16'h0002; #10;
+
+    $finish;
+  end
+
+endmodule
