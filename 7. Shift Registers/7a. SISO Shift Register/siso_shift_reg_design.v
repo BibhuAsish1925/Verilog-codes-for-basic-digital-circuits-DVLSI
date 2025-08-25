8e142dd3ad1df4ed7bfe0306c93fe16acc410f2b
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-08-25 19:49:59 +0530

    Add files via upload

--- 7. Shift Registers/7a. SISO Shift Register/siso_shift_reg_design.v
+++ 7. Shift Registers/7a. SISO Shift Register/siso_shift_reg_design.v
@@ -1,16 +1,16 @@
 module siso_4bit_shift_reg(
   input din,clk,
-  output [3:0]q,
+  output dout,   // O/P of SISO is a single output from last stage
   output [3:0]q_bar
 );
-  wire [3:0]q_int; //1st separate q and q_int since q cannot be used as wire and already declared output
+  wire [3:0]q_int;
   
   D_flip_flop d3(din,clk,q_int[3],q_bar[3]);
   D_flip_flop d2(q_int[3],clk,q_int[2],q_bar[2]);
   D_flip_flop d1(q_int[2],clk,q_int[1],q_bar[1]);
   D_flip_flop d0(q_int[1],clk,q_int[0],q_bar[0]);
   
-  assign q=q_int;  //lastly the q and q_int are made same in order to get output in q form
+  assign dout=q_int[0];  //lastly the dout and q_int[0] are made same in order to get output in dout single output form
   
 endmodule
   
