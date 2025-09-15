bf7d94597ef7101bdb755c0c786555b52d6d5df8
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:28:01 +0530

    Update easy_bcd_adder_design.v

--- 2. Adders and Subtractors/2f. BCD Adder/Easy-way BCD adder/easy_bcd_adder_design.v
+++ 2. Adders and Subtractors/2f. BCD Adder/Easy-way BCD adder/easy_bcd_adder_design.v
@@ -1,18 +1,19 @@
-module bcd_adder (
-  input [3:0]A,B,
+module BCD_adder (
+  input [3:0] A, B,
   input Cin,
-  output [3:0]Sum,
-  output Cout
+  output reg [3:0] Sum,
+  output reg Cout
 );
-  reg [4:0]sx;
+  reg [4:0] sx;
 
   always @(*) begin
     sx = A + B + Cin;
 
     if (sx > 9)
-      sx = sx + 5'd6;  
+      sx = sx + 5'd6;
+
+    Sum  = sx[3:0];
+    Cout = sx[4];
   end
 
-  assign Sum  = sx[3:0];
-  assign Cout = sx[4];       
 endmodule
