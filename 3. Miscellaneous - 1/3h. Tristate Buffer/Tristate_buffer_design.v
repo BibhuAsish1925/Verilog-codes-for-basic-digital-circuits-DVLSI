3c352309fe5c53499d453685cee06831959c566b
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:13:10 +0530

    Update Tristate_buffer_design.v

--- 3. Miscellaneous - 1/3h. Tristate Buffer/Tristate_buffer_design.v
+++ 3. Miscellaneous - 1/3h. Tristate Buffer/Tristate_buffer_design.v
@@ -1,7 +1,7 @@
-module tristate_buffer(in, en, out);
-  input  in, en;
-  output out;
-
-  assign out = en ? in : 1'bz;  //z is the high-z state of tristate buffer 
-  
+module tristate_buffer(
+  input  in, en,
+  output out
+    );
+    
+    assign out = en ? in : 1'bz;  //z is the high-z state of tristate buffer 
 endmodule
