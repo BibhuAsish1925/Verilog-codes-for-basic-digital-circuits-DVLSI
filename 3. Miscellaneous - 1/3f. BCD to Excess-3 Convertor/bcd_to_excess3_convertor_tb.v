cb5a24a90f28d3626b6c211ed8b5a5aa14168101
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:15:23 +0530

    Update bcd_to_excess3_convertor_tb.v

--- 3. Miscellaneous - 1/3f. BCD to Excess-3 Convertor/bcd_to_excess3_convertor_tb.v
+++ 3. Miscellaneous - 1/3f. BCD to Excess-3 Convertor/bcd_to_excess3_convertor_tb.v
@@ -1,22 +1,21 @@
-module tb1;
+module bcd_to_excess3_convertor_tb();
+
   reg  [3:0] b;
   wire [3:0] x;
 
-  bcd_x3_conv uut(b, x);
+  bcd_to_excess3_convertor uut(b, x);
 
   initial begin
-    
-    $dumpfile("dump.vcd"); $dumpvars;
    
-    b = 4'b0000; #10;
-    b = 4'b0001; #10;
-    b = 4'b0010; #10;
-    b = 4'b0011; #10;
-    b = 4'b0100; #10;
-    b = 4'b0101; #10;
-    b = 4'b0110; #10;
-    b = 4'b0111; #10;
-    b = 4'b1000; #10;
+    b = 4'b0000; #100;
+    b = 4'b0001; #100;
+    b = 4'b0010; #100;
+    b = 4'b0011; #100;
+    b = 4'b0100; #100;
+    b = 4'b0101; #100;
+    b = 4'b0110; #100;
+    b = 4'b0111; #100;
+    b = 4'b1000; #100;
 
   end
 endmodule
