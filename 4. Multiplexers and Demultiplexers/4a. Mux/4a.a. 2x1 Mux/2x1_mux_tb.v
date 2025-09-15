be70a9353f85e76dd553730242c675312739ab93
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 18:07:14 +0530

    Update 2x1_mux_tb.v

--- 4. Multiplexers and Demultiplexers/4a. Mux/4a.a. 2x1 Mux/2x1_mux_tb.v
+++ 4. Multiplexers and Demultiplexers/4a. Mux/4a.a. 2x1 Mux/2x1_mux_tb.v
@@ -1,19 +1,18 @@
-module tb1();
+module mux_2x1_tb();
+
   reg i1,i0,s; 
   wire out;
-  int i;
+  integer i;
   
   mux_2x1 uut(i1,i0,s,out);
   
   initial begin
     
-    $dumpfile("dump.vcd"); $dumpvars;
-    
     {s,i1,i0}=0;
     for(i=0;i<=8;i=i+1)
       begin
         {s,i1,i0}=i;
-        #10;
+        #100;
       end
   end
 endmodule
