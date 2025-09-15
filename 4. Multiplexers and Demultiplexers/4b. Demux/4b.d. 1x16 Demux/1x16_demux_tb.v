76b01016eb02e0b78b6af03674ede28fd3124571
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 19:40:42 +0530

    Update 1x16_demux_tb.v

--- 4. Multiplexers and Demultiplexers/4b. Demux/4b.d. 1x16 Demux/1x16_demux_tb.v
+++ 4. Multiplexers and Demultiplexers/4b. Demux/4b.d. 1x16 Demux/1x16_demux_tb.v
@@ -1,19 +1,18 @@
-module tb1();
+module demux_1x16_tb();
   reg in, en;
   reg [3:0]s; 
   wire [15:0]y;
-  int a;
+  integer a;
   
-  demux_1x16 uut(in,en,s,y);
+  demux_1x16 uut(.in(in),.en(en),.s(s),.y(y));
   
   initial begin
     
-    $dumpfile("dump.vcd"); $dumpvars;
     {en,s,in}=0;
     for(a=0;a<=70;a=a+1)
       begin
         {en,s,in}=a; #5;
       end
-    
+    $finish;
   end
 endmodule
