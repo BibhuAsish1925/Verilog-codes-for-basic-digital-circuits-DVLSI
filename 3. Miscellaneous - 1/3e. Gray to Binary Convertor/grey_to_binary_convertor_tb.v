d191dfad62bd1ac94dd7b31483570c6a783243bb
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:16:35 +0530

    Update grey_to_binary_convertor_tb.v

--- 3. Miscellaneous - 1/3e. Gray to Binary Convertor/grey_to_binary_convertor_tb.v
+++ 3. Miscellaneous - 1/3e. Gray to Binary Convertor/grey_to_binary_convertor_tb.v
@@ -1,20 +1,19 @@
-module tb();
+module gray_binary_conv_tb();
+
   reg  [3:0] gray;
   wire [3:0] bin;
 
   gray_binary_conv uut (gray, bin);
 
   initial begin
-    
-    $dumpfile("gray_conv.vcd");
-    $dumpvars(0, tb);
 
-    gray = 4'b1010; #10;
-    gray = 4'b1000; #10; 
-    gray = 4'b0101; #10;  
-    gray = 4'b1110; #10;
-    gray = 4'b0010; #10; 
+    gray = 4'b1010; #100;
+    gray = 4'b1000; #100; 
+    gray = 4'b0101; #100;  
+    gray = 4'b1110; #100;
+    gray = 4'b0010; #100; 
     $finish;
     
   end
+
 endmodule
