a1370e94d1466c038ea8ce5cea27c7fd42ac2427
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:28:27 +0530

    Update Bit_4_adder_cum_subtractor_tb.v

--- 2. Adders and Subtractors/2e. 4-bit Adder cum Subtractor/Bit_4_adder_cum_subtractor_tb.v
+++ 2. Adders and Subtractors/2e. 4-bit Adder cum Subtractor/Bit_4_adder_cum_subtractor_tb.v
@@ -1,5 +1,4 @@
-// TestBench:
-module tb2();
+module bit_4_adder_cum_subtractor_tb();
   reg [3:0]a,b; 
   reg cbin;
   wire [3:0]sd;
@@ -12,11 +11,13 @@ module tb2();
     $dumpvars;
     
     //test cases taken manually total 2^9=512 cases possible
-    a=4'b0000; b=4'b0101; cbin=0; #10;
-    a=4'b0110; b=4'b0101; cbin=1; #10;
-    a=4'b0110; b=4'b1111; cbin=0; #10;
-    a=4'b1111; b=4'b1111; cbin=1; #10;
-    a=4'b0101; b=4'b0110; cbin=1; #10;
+    a=4'b0000; b=4'b0101; cbin=0; #100;
+    a=4'b0110; b=4'b0101; cbin=1; #100;
+    a=4'b0110; b=4'b1111; cbin=0; #100;
+    a=4'b1111; b=4'b1111; cbin=1; #100;
+    a=4'b0101; b=4'b0110; cbin=1; #100;
+    
+    $stop;
 
   end
 endmodule
