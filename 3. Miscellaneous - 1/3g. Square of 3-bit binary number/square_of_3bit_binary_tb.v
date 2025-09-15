cbfd9d49e2a35693f4a74ae0faa33033c9b100f3
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:13:40 +0530

    Update square_of_3bit_binary_tb.v

--- 3. Miscellaneous - 1/3g. Square of 3-bit binary number/square_of_3bit_binary_tb.v
+++ 3. Miscellaneous - 1/3g. Square of 3-bit binary number/square_of_3bit_binary_tb.v
@@ -1,21 +1,21 @@
-module tb3;
+module square_of_3bit_binary_tb();
+
   reg  [2:0] in;
   wire [5:0] out;
 
-  squares_3bit dut(in, out);
+  square_of_3bit_binary dut(.in(in), .out(out));
 
   initial begin
-    $dumpfile("dump.vcd"); $dumpvars;
     $monitor("Time=%0t | in=%b | out=%b", $time, in, out);
 
-    in = 3'b000; #10;
-    in = 3'b001; #10;
-    in = 3'b010; #10;
-    in = 3'b011; #10;
-    in = 3'b100; #10;
-    in = 3'b101; #10;
-    in = 3'b110; #10;
-    in = 3'b111; #10;
+    in = 3'b000; #100;
+    in = 3'b001; #100;
+    in = 3'b010; #100;
+    in = 3'b011; #100;
+    in = 3'b100; #100;
+    in = 3'b101; #100;
+    in = 3'b110; #100;
+    in = 3'b111; #100;
 
     $finish;
   end
