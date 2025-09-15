fba373859991500b548c9cc8421865b8b9f5d81e
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 19:23:32 +0530

    Update 1x4_demux_tb.v

--- 4. Multiplexers and Demultiplexers/4b. Demux/4b.b. 1x4 Demux/1x4_demux_tb.v
+++ 4. Multiplexers and Demultiplexers/4b. Demux/4b.b. 1x4 Demux/1x4_demux_tb.v
@@ -1,10 +1,10 @@
-module tb1();
+module demux_1x4_tb();
   reg in, en;
   reg [1:0]s; 
   wire [3:0]y;
-  int a;
+  integer a;
   
-  demux_1x4 uut(in,en,s,y);
+  demux_1x4 uut(.in(in),.en(en),.s(s),.y(y));
   
   initial begin
     
@@ -12,8 +12,8 @@ module tb1();
     {en,s,in}=0;
     for(a=0;a<=20;a=a+1)
       begin
-        {en,s,in}=a; #10;
+        {en,s,in}=a; #100;
       end
-    
+    $finish;
   end
 endmodule
