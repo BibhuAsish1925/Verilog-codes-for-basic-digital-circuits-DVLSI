27d231d4b9439aa86a3ce6d711438ca6ac7d29c9
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 20:53:40 +0530

    Update 4x2_encoder_tb.v

--- 5. Encoders and Decoders/5a. Encoders/5a.a. 4x2 Encoder/4x2_encoder_tb.v
+++ 5. Encoders and Decoders/5a. Encoders/5a.a. 4x2 Encoder/4x2_encoder_tb.v
@@ -1,25 +1,24 @@
-module tb1();
-  reg [3:0]i; 
+module encoder_4x2_tb();
+reg [3:0]i; 
   wire [1:0]out;
   //int a;
   
-  encoder_4x2 uut(i,out);
+  encoder_4x2 uut(.i(i),.out(out));
   
   initial begin
-    
-    $dumpfile("dump.vcd"); $dumpvars;
+
     i=4'b0001; 
-    #5;
+    #100;
     i=4'b0010; 
-    #5;
+    #100;
     i=4'b0100; 
-    #5;
+    #100;
     i=4'b1000; 
-    #5;
+    #100;
     i=4'b0010; 
-    #5;
+    #100;
     i=4'b0100; 
-    #5;
-    
+    #100;
+     $finish;
   end
 endmodule
