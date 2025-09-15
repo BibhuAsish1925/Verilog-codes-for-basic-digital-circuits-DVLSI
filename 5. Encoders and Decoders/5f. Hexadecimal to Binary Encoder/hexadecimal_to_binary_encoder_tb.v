af88a50a264b27e7793d8ec2c20fec0e6d79d929
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 22:33:32 +0530

    Update hexadecimal_to_binary_encoder_tb.v

--- 5. Encoders and Decoders/5f. Hexadecimal to Binary Encoder/hexadecimal_to_binary_encoder_tb.v
+++ 5. Encoders and Decoders/5f. Hexadecimal to Binary Encoder/hexadecimal_to_binary_encoder_tb.v
@@ -1,4 +1,4 @@
-module tb;
+module hexadecimal_binary_tb();
 
   reg  [15:0] in;
   wire [3:0]  out;
@@ -6,16 +6,13 @@ module tb;
   hexadecimal_binary uut (.in(in),.out(out));
 
   initial begin
-    $dumpfile("hex_encoder.vcd");
-    $dumpvars(0, tb);
 
-    in = 16'h0000; #10;
-    in = 16'h8000; #10;
-    in = 16'h0400; #10;
-    in = 16'h0002; #10;
+    in = 16'h0000; #100;
+    in = 16'h8000; #100;
+    in = 16'h0400; #100;
+    in = 16'h0002; #100;
 
     $finish;
   end
 
 endmodule
-
