f8214561619a29de399ca0e3e0a1fd102be34a8b
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:27:32 +0530

    Update easy_bcd_adder_tb.v

--- 2. Adders and Subtractors/2f. BCD Adder/Easy-way BCD adder/easy_bcd_adder_tb.v
+++ 2. Adders and Subtractors/2f. BCD Adder/Easy-way BCD adder/easy_bcd_adder_tb.v
@@ -1,21 +1,22 @@
-module testbench;
+module BCD_adder_tb();
   reg  [3:0] A, B;
-  reg        Cin;
+  reg  Cin;
   wire [3:0] Sum;
-  wire       Cout;
+  wire  Cout;
 
-  bcd_adder UUT (A, B, Cin, Sum, Cout);
+  BCD_adder UUT (A, B, Cin, Sum, Cout);
 
   initial begin
     $dumpfile("dump.vcd"); $dumpvars;
     $monitor("A=%d B=%d Cin=%b | Sum=%d Cout=%b", A, B, Cin, Sum, Cout);
 
-    A = 4'd3; B = 4'd5; Cin = 0; #10;
-    A = 4'd4; B = 4'd6; Cin = 0; #10;
-    A = 4'd9; B = 4'd9; Cin = 0; #10;
-    A = 4'd2; B = 4'd7; Cin = 1; #10;
-    A = 4'd0; B = 4'd9; Cin = 1; #10;
+    A = 4'd3; B = 4'd5; Cin = 0; #100;
+    A = 4'd4; B = 4'd6; Cin = 0; #100;
+    A = 4'd9; B = 4'd9; Cin = 0; #100;
+    A = 4'd2; B = 4'd7; Cin = 1; #100;
+    A = 4'd0; B = 4'd9; Cin = 1; #100;
 
     $finish;
   end
+  
 endmodule
