ba167c3eca03a2fb7dc315e736ddd00cb87b66b6
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 22:04:32 +0530

    Update priority_encoder_design.v

--- 5. Encoders and Decoders/5c. Priority Encoder/priority_encoder_design.v
+++ 5. Encoders and Decoders/5c. Priority Encoder/priority_encoder_design.v
@@ -1,10 +1,14 @@
-module priority_encoder(
-  input [3:0] in,
-  output [1:0] out
+module priority_encoder (
+  input  [3:0] in,
+  output [1:0] out,
+  output valid
 );
-  
-  assign out[1]= in[2]|in[3];
-  assign out[0]=in[3]|((~in[2])&in[0]);
+
+  // Priority encoding logic
+  assign out[1] = in[2] | in[3];
+  assign out[0] = in[3] | ((~in[2]) & in[1]);
+
+  // Valid bit: high if any input is 1
+  assign valid = |in;  // Equivalent to (in[0] | in[1] | in[2] | in[3])
+
 endmodule
-
-
