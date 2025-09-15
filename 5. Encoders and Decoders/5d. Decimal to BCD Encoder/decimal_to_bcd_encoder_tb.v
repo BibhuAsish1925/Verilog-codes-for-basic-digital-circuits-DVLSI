2e32abee4d6dcd9ba1c3083518cb6257e222a359
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 22:16:55 +0530

    Update decimal_to_bcd_encoder_tb.v

--- 5. Encoders and Decoders/5d. Decimal to BCD Encoder/decimal_to_bcd_encoder_tb.v
+++ 5. Encoders and Decoders/5d. Decimal to BCD Encoder/decimal_to_bcd_encoder_tb.v
@@ -1,17 +1,16 @@
-module tb();
+module decimal_bcd_encoder_tb();
+
   reg [9:0] in;
   wire [3:0] out;
   
-  decimal_bcd_encoder uut (in,out);
+  decimal_bcd_encoder uut(.in(in),.out(out));
   
   initial begin
     
-    $dumpfile("dump.vcd"); $dumpvars;
-    
-    in=10'b000000100; #10
-    in=10'b100000000; #10
-    in=10'b000010000; #10
-    in=10'b000001010; #10
+    in=10'b000000100; #100;
+    in=10'b100000000; #100;
+    in=10'b000010000; #100;
+    in=10'b000001010; #100;
     
     $finish;
   end
