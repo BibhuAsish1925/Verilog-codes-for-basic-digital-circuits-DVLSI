294b92cf63f46f7126b49705d933a854a2816b26
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 22:03:45 +0530

    Update priority_encoder_tb.v

--- 5. Encoders and Decoders/5c. Priority Encoder/priority_encoder_tb.v
+++ 5. Encoders and Decoders/5c. Priority Encoder/priority_encoder_tb.v
@@ -1,16 +1,17 @@
-module tb( );
+module priority_encoder_tb();
+
   reg [3:0] in;
   wire [1:0] out;
   
-  priority_encoder uut(in,out);
+  priority_encoder uut(.in(in),.out(out));
   
   initial begin
-    in=4'b0000; #5;
-    in=4'b1001; #10;
-    in=4'b0011; #10
-    in=4'b1101; #10
-    in=4'b0101; #10
-    in=4'b0001; #10
+    in=4'b0000; #100;
+    in=4'b1001; #100;
+    in=4'b0011; #100;
+    in=4'b1101; #100;
+    in=4'b0101; #100;
+    in=4'b0001; #100;
     $finish;
 
   end
