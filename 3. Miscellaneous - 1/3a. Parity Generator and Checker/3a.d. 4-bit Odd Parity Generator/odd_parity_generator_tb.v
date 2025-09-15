5cb05b69637e702859b8d6654e6f0e5417b62a92
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-15 17:24:25 +0530

    Update odd_parity_generator_tb.v

--- 3. Miscellaneous - 1/3a. Parity Generator and Checker/3a.d. 4-bit Odd Parity Generator/odd_parity_generator_tb.v
+++ 3. Miscellaneous - 1/3a. Parity Generator and Checker/3a.d. 4-bit Odd Parity Generator/odd_parity_generator_tb.v
@@ -1,7 +1,7 @@
 module bit_4_odd_parity_gen_tb();
   reg b0,b1,b2,b3;
   wire y;
-  int i;
+  integer i;
   
   bit_4_odd_parity_gen uut(.b0(b0),.b1(b1),.b2(b2),.b3(b3),.y(y));
   
@@ -11,8 +11,7 @@ module bit_4_odd_parity_gen_tb();
     {b3,b2,b1,b0}=0;
     for(i=0; i<=15; i=i+1)
       begin
-        {b3,b2,b1,b0}=i; #10;
+        {b3,b2,b1,b0}=i; #50;
       end
   end
 endmodule
-    
