9f84d37a16b6ab2481a9499a5dd5fa935dabe14d
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-16 11:38:52 +0530

    Update RS_latch_design.v

--- 6. Latches & Flip-Flops/6a. RS - Latch/RS_latch_design.v
+++ 6. Latches & Flip-Flops/6a. RS - Latch/RS_latch_design.v
@@ -1,27 +1,14 @@
-module RS_latch(
-  input s,r,
-  output reg q,q_bar
+module RS_latch (
+    input  wire s, r,
+    output reg  q, q_bar
 );
-  
   //remember it is prefered to consider RS-latch not SR-latch when NAND gates are used**
-  
-  always @({s,r})
-    begin
-      if (s==1'b0 && r==1'b0)
-        begin
-          q<=1'b1;  q_bar<=1'b1;
-        end
-      else if (s==1'b1 && r==1'b0)
-        begin
-          q<=1'b0;  q_bar<=1'b1;
-        end
-      else if (s==1'b0 && r==1'b1)
-        begin
-          q<=1'b1;  q_bar<=1'b0;
-        end
-      else if (s==1'b1 && r==1'b1)
-        begin
-          q<=1'bx;  q_bar<=1'bx;  // x is dont care
-        end
+    always @(*) begin
+        case ({s,r})
+            2'b00: begin q = 1; q_bar = 1; end
+            2'b01: begin q = 1; q_bar = 0; end
+            2'b10: begin q = 0; q_bar = 1; end
+            2'b11: begin q = 1'bx; q_bar = 1'bx; end
+        endcase
     end
 endmodule
