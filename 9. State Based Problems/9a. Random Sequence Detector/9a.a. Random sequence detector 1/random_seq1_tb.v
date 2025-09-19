411ec70b94b2ad9ecbe507d7ccacdd3be0c1b9ab
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-19 12:19:07 +0530

    Add files via upload

---
+++ 9. State Based Problems/9a. Random Sequence Detector/9a.a. Random sequence detector 1/random_seq1_tb.v
@@ -0,0 +1,26 @@
+module tb;
+
+  reg clk;
+  //reg [2:0]t;
+  wire [2:0] qout;
+
+  // Instantiate the counter
+  Random_seq_detector1 uut (.clk(clk),.qout(qout));
+
+  // Clock generation: 10ns period
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $dumpfile("dump.vcd"); $dumpvars;
+    $display("Time\tclk\tt\tqout");
+    //$monitor("%0t\t%b\t%b\t%d", $time, clk, t, qout);
+    
+    #100;
+
+    $finish;
+  end
+
+endmodule
