a3aa36383c457a026d1c64df24677836f10e5f74
Bibhu Asish Panda <bibhuap2003@gmail.com>
2025-09-20 22:08:01 +0530

    Update 1x4_demux_design.v

--- 4. Multiplexers and Demultiplexers/4b. Demux/4b.b. 1x4 Demux/1x4_demux_design.v
+++ 4. Multiplexers and Demultiplexers/4b. Demux/4b.b. 1x4 Demux/1x4_demux_design.v
@@ -16,8 +16,7 @@ module demux_1x4(
   //structural using 1x2 demux
   demux_1x2 d1(in,en,s[1],w[1:0]);
   demux_1x2 d2(w[0],en,s[0],y[1:0]);
-  demux_1x2 d3(w[1],en,s[0],y[3:2]);
-  
+  demux_1x2 d3(w[1],en,s[0],y[3:2]);  
 endmodule
   
 //for structural purpose
@@ -25,10 +24,10 @@ module demux_1x2(
   input in,en,s,   //en is for ckt on and off
   output [1:0]y
 );
-
   assign y[0]= en&~s&in;
   assign y[1]= en&s&in;
    
 endmodule
  
+
   
